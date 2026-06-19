1f3557b994b147af857cc7e548675ac029b582ec
praharshita <24wh1a6627@bvrithyderabad.edu.in>
2026-06-20 00:19:04 +0530

    toggle

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express =require("express");
 const app=express();
 const port=process.env.PORT || 6700;
-//to tell app,where static files- html/css/js are at
-app.use(express.static("frontend"));
+//to tell app,where static files- html/css/js(files that don't need server processing) are at
+app.use(express.static("frontend")); //says that "All website files are inside frontend folder".
 
 app.listen(port,function(){
     console.log("App running in http://localhost:" +port);
