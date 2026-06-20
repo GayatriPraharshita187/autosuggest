ea70495db7655834fc4c4196f10b402a2d07f482
praharshita <24wh1a6627@bvrithyderabad.edu.in>
2026-06-20 10:40:06 +0530

    randomuser

--- frontend/js/toggle.js
+++ frontend/js/toggle.js
@@ -7,7 +7,7 @@ toggleButton.addEventListener("click", function () {
 
     if (genderText.textContent === "Male") {
 
-        image.src="../images/jane.png";
+        image.src = "../images/jane.png";
         nameText.textContent = "Jane Doe";
         genderText.textContent = "Female";
 
@@ -20,3 +20,26 @@ toggleButton.addEventListener("click", function () {
     }
 
 });
+
+// var users=[
+//     {
+//         "name": "John Doe";
+//         "gender": "Male";
+//         "image": "/images/john.pgn";
+//     },
+//     {
+//         "name":"Jane Doe";
+//         "gender": "Female";
+//         "image":"/images/jane.png";
+//     }
+// ]
+// var curid=0;
+// function toggle(){
+//       //toggle curid from o -> 1 & 1-> 0
+//     curid=(curid+1)%2
+//       //toggle the rendered user details
+
+//       //image
+//     var user=users[curid];
+//     document.getElementById("user-image").src=""
+// }
